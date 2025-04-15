7df971489b7d9b44ca455930394c68a6a2a9d974
Auriane Martel <auriane.martel@gmail.com>
2025-04-15 10:43:08 +0200

    Backend : movies : commande post

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -3,10 +3,15 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', function (req, res) {
-    console.log("Coucou");
+    console.log("Current : get api/movies");
     res.json(null);
 });
 
+router.post('/new', function (req, res) {
+    const { titre, date } = req.body;
+    console.log(req.body);
+    res.json({ message: "OK" })
+});
 
 
 export default router;
